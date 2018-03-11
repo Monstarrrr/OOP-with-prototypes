416520bf9e4b4514c3683734892cba75ffd1305e
Monstar <31903668+Monstarrrr@users.noreply.github.com>
2018-03-11 04:29:43 +0100

    +monsters && +functions && err line45

--- js/hero.js
+++ js/hero.js
@@ -1,24 +1,59 @@
+//Prototype Hero
 var Hero = {
-    name: "",
-    health: 0,
-    strength: 0,
-    xp: 0,
 
-    describe: function () {
-        var describe = this.name + " has " + this.health + " HP, " + this.strength + " SP and " + this.xp + " XP.";
-        return describe;
+    //Initializing Hero
+    initHero: function (name, health, strength) {
+        this.name = name;
+        this.health = health;
+        this.strength = strength;
+        this.xp = 0;
     }
 };
 
-var Hero1 = Object.create(Hero);
-Hero1.name = "Kestrel";
-Hero1.health = 110;
-Hero1.strength = 30;
 
-var Hero2 = Object.create(Hero);
-Hero2.name = "Ardan";
-Hero2.health = 200;
-Hero2.strength = 10;
+//Set Player as a Hero
+var Player = Object.create(Hero);
 
-console.log(Hero1.describe());
-console.log(Hero2.describe());
+//Add initPlayer function to Player
+Player.initPlayer = function (name, health, strength) {
+    this.initPlayer(name, health, strength);
+    this.xp = 0;
+};
+//Add description function to Player
+Player.description = function () {
+    var describe = this.name + " has " + this.health + " HP, " + this.strength + " SP and " + this.xp + " XP.";
+    return describe;
+};
+
+
+//Set Enemy as a Hero
+var Enemy = Object.create(Hero);
+
+//Add initEnemy function to Enemy
+Enemy.initEnemy = function (name, health, strength, breed, value) {
+    this.initPlayer(name, health, strength);
+    this.breed = breed;
+    this.value = value;
+};
+
+
+
+// --- //
+
+
+//Create Player 1
+var player1 = Object.create(Player);
+player1.initPlayer("Kestrel", 110, 30);
+
+//Create Player 2
+var player2 = Object.create(Player);
+player1.initPlayer("Ardan", 250, 10);
+
+console.log("Welcome to the game! Here are our heroes :")
+console.log(player1.describe());
+console.log(player2.describe());
+
+var monster = Object.create(Enemy);
+monster.initEnemy("Baron", 100, 40, "Mecha", 10);
+
+console.log("A monster show up: It's a " + monster.breed + " called " + monster.name);
